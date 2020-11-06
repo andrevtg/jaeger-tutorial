826328261d8a524e535bd55e0766f6f49489723f
André Fernandes <andre@vertigo.com.br>
2020-11-06 13:53:00 -0300

    versao ok com tracing atualizado

--- docker/app/JaegerTutorial/bin/start.sh
+++ docker/app/JaegerTutorial/bin/start.sh
@@ -2,7 +2,8 @@
 
 
 export LIBS=/app/JaegerTutorial/libs
-export APP=com.egoebelbecker.jaegertutorial.TutorialApplication
+#export APP=com.egoebelbecker.jaegertutorial.TutorialApplication
+export APP=org.springframework.boot.loader.JarLauncher
 export LOG_DIR=${APP_HOME}/logs
 export CLASSPATH=${CLASSPATH}:${LIBS}/*
 ${JAVA_HOME}/bin/java -enableassertions -cp ${CLASSPATH} ${LOGBACKCFG} ${APP}
